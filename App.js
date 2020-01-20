9f9f0219f0c4323d4e0ddf8cbccbbb3a80970f15
helenmaia <51785546+helenmaia@users.noreply.github.com>
2020-01-20 18:50:11 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+import FoodListScreen from './src/screens/FoodListScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import FoodFormScreen from './src/screens/FoodFormScreen';
+import FoodDetailScreen from './src/screens/FoodDetailScreen';
+
+
+
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+const AppStack = createStackNavigator({
+  FoodList: FoodListScreen,
+  FoodForm: FoodFormScreen,
+  FoodDetail: FoodDetailScreen,
+  
+  
+});
+
+
+const AuthNavigator = createStackNavigator({
+  LoginRoute: {
+    screen: LoginScreen,
+    navigationOptions: () => ({
+      header: null
+    })
+  }
+});
+
+
+
+const AppContainer = createAppContainer(createSwitchNavigator(
+  {
+
+    App: AppStack,
+    Auth: AuthNavigator
+  },
+  {
+    initialRouteName: 'Auth',
+  }
+));
+
+export default class App extends Component {
+  render() {
+    return (
+      <AppContainer
+        screenProps={{ appName: 'CardAPP' }}
+      />
+    )
+  }
+}
